d30aad28234277418c8762f89bb405dc12d4e65f
https://github.com/Ahil0764057888/programming.git <mohammedahil948@gmail.com>
2024-02-06 09:07:29 -0800

    Initial commit

---
+++ HRM.py
@@ -0,0 +1,26 @@
+class Employeee:
+    def __init__ (self, Emp_id, Emp_name, Emp_dep, Emp_contact, Emp_appoinmentdate, Emp_status):
+        self.Emp_id= Emp_id
+        self.Emp_name=Emp_name
+        self.Emp_dep=Emp_dep
+        self.Emp_contact=Emp_contact
+        self.Emp_appoinmentdate=Emp_appoinmentdate
+        self.Emp_status=Emp_status
+
+    def add_employee(self,Emp_id,Emp_name,Emp_dep,Emp_contact,Emp_apponmentdate,Emp_status):
+        pass
+
+    def __ViewEmpInfo__(self):
+        pass
+
+    def __UpdateEmpInfo__(self):
+        pass
+
+    def __leaveRequest__(self):
+        pass
+
+    def __ViewcompanyPolicies__(self):
+        pass
+
+    def __AccessworkShedule__(self):
+        pass
